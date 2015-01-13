1cde4e5fb05177a1d27b59ba331d9da1fdc552e7
Stuart Berg <bergs@janelia.hhmi.org>
2015-01-13 11:54:11 -0500

    distance_histogram: Optionally include "negative" (i.e. missing) distances in histogram, and also include missing count in plot title.

--- skeleton_synapses/distance_histogram.py
+++ skeleton_synapses/distance_histogram.py
@@ -4,14 +4,17 @@ import numpy
 import matplotlib.pyplot as plt
 from skeleton_utils import CSV_FORMAT
 
-def distance_histogram( detection_csv_path ):
+def distance_histogram( detection_csv_path, include_negative_distances=False ):
     distances = []
+    missing = 0
     with open( detection_csv_path, 'r' ) as detection_file:
         csv_reader = csv.DictReader(detection_file, **CSV_FORMAT)
         for row in csv_reader:
             distance = float( row["distance"] )
-            if distance > 0.0:
+            if distance >= 0.0 or include_negative_distances:
                 distances.append( distance )
+            elif not include_negative_distances:
+                missing += 1
 
     distances = numpy.asarray( distances, dtype=numpy.float32 )
     
@@ -19,7 +22,11 @@ def distance_histogram( detection_csv_path ):
 
     plt.xlabel('Membrane "Distance"')
     plt.ylabel('Detections')
-    plt.title( os.path.split(detection_csv_path)[1] )
+    if missing > 0:
+        plt.title( os.path.split(detection_csv_path)[1] + " ({} shown, {} missing)".format( len(distances), missing ) )
+    else:
+        plt.title( os.path.split(detection_csv_path)[1] + " ({} shown)".format( len(distances) ) )
+
     plt.show()
 
 if __name__ == "__main__":
@@ -28,11 +35,14 @@ if __name__ == "__main__":
 
     DEBUG_ARGS = False
     if DEBUG_ARGS:
-        sys.argv.append( "/Users/bergs/Documents/workspace/skeleton_synapses/test_skeletons/connected_node_distances_18689.csv" )
+        #sys.argv.append( "/Users/bergs/Documents/workspace/skeleton_synapses/test_skeletons/connected_node_distances_18689.csv" )
+        #sys.argv.append( "/Users/bergs/Documents/workspace/skeleton_synapses/test_skeletons/connected_node_distances_133465.csv" )
+        #sys.argv.append( "/Users/bergs/Documents/workspace/skeleton_synapses/test_skeletons/connected_node_distances_163751.csv" )
+        sys.argv.append( "/Users/bergs/Documents/workspace/skeleton_synapses/test_skeletons/connected_node_distances_94835.csv" )
     
     parser = argparse.ArgumentParser()
     parser.add_argument("detection_csv_file")
     parsed_args = parser.parse_args()
     
     sys.exit( distance_histogram(parsed_args.detection_csv_file) )
-    
+
