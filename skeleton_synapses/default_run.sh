56f4564477c0343457ca6c6dcbdc960449eb854f
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-03-16 15:45:29 -0400

    Update run scripts for new signatures

--- skeleton_synapses/default_run.sh
+++ skeleton_synapses/default_run.sh
@@ -4,8 +4,7 @@ cred_path="credentials_real.json"
 stack_id=1
 skel_id=11524047
 
-./locate_synapses.py ${cred_path} ${stack_id} ${skel_id} ${project_dir}/projects/full-vol-autocontext.ilp \
-${project_dir}/projects/multicut/L1-CNS-multicut.ilp ${project_dir}/skeletons;
+./locate_synapses.py ${cred_path} ${stack_id} ${skel_id} ${project_dir};
 
 ./results_to_volume.py ${cred_path} ${stack_id} ${skel_id} ${project_dir}/skeletons \
 ${project_dir}/synapse_volume.hdf5;
