78d8c0596d59cdf02f5f8bce57239beeae9a1a9e
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-03-21 18:17:40 -0400

    timing in run scripts

--- skeleton_synapses/default_run.sh
+++ skeleton_synapses/default_run.sh
@@ -8,7 +8,6 @@ force=1
 timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 
 log_dir=./logs/${timestamp}
-
 mkdir -p ${log_dir}
 
 source ./set_env_vars.sh
@@ -17,9 +16,15 @@ script_path=`realpath $0`
 cp ${script_path} ${log_dir}/run_script.sh
 cp ./set_env_vars.sh ${log_dir}/set_env_vars.sh
 
+echo "Started at ${timestamp}" > ${log_dir}/time.txt
+
 ./locate_synapses.py ${cred_path} ${stack_id} ${skel_id} ${project_dir} -f ${force} 2>&1 | tee ${log_dir}/locate_synapses.txt;
+
+echo "Segmentation finished at $(date +"%Y-%m-%d_%H:%M:%S")" >> ${log_dir}/time.txt
 
 ./results_to_volume.py ${cred_path} ${stack_id} ${skel_id} ${project_dir}/skeletons \
 ${project_dir}/synapse_volume.hdf5 -f ${force} 2>&1 | tee ${log_dir}/results_to_volume.txt;
+
+echo "Data committing finished at $(date +"%Y-%m-%d_%H:%M:%S")" >> ${log_dir}/time.txt
 
 echo "FINISHED"
