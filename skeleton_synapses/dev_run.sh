b4c8f8aebcc56ea42626b998e7da4254176ca07f
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-03-21 17:09:42 -0400

    improved logging and env var replicability

--- skeleton_synapses/dev_run.sh
+++ skeleton_synapses/dev_run.sh
@@ -3,8 +3,21 @@ project_dir="../projects-2017/L1-CNS"
 cred_path="credentials_dev.json"
 stack_id=1
 skel_id=11524047
+force=1
 
-./locate_synapses.py ${cred_path} ${stack_id} ${skel_id} ${project_dir};
+timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
+
+log_dir=./logs/${timestamp}
+
+mkdir -p ${log_dir}
+
+source ./set_env_vars.sh
+
+script_path=`realpath $0`
+cp ${script_path} ${log_dir}/run_script.sh
+cp ./set_env_vars.sh ${log_dir}/env_script.sh
+
+./locate_synapses.py ${cred_path} ${stack_id} ${skel_id} ${project_dir} -f &> ${log_dir}/locate_synapses.txt;
 
 ./results_to_volume.py ${cred_path} ${stack_id} ${skel_id} ${project_dir}/skeletons \
-${project_dir}/synapse_volume.hdf5;
+${project_dir}/synapse_volume.hdf5 -f &> ${log_dir}/results_to_volume.txt;
