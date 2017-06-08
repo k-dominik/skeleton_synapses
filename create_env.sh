cf8e7be741dd259ade455b9de77c683797cbcbbe
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-08 18:29:05 -0400

    script to create clean environment

---
+++ create_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+ENV_NAME=$1
+
+conda create -n ${ENV_NAME} python=2.7 ipython
+source activate ${ENV_NAME}
+conda install -c ilastik ilastik-everything
+
+easy_install -U pip
+pip install -r requirements
