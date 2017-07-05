c04fa8ddf142a8d503bd5e7271e57193edecc6b1
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-07-05 17:13:10 -0400

    update env creation script with fixes

--- create_env.sh
+++ create_env.sh
@@ -4,9 +4,27 @@ set -e
 
 ENV_NAME=$1
 
-conda create -n ${ENV_NAME} python=2.7 ipython
-source activate ${ENV_NAME}
-conda install -c ilastik ilastik-everything
+if [ -z "$ENV_NAME" ]; then
+    >&2 echo "Pass environment name as first argument"
+    exit 1
+fi
 
+# delete environment if it exists, noop if not
+conda remove -y --name ${ENV_NAME} --all || :
+
+# create, activate, and install ilastik in environment
+conda create -y -n ${ENV_NAME} python=2.7 ipython
+source activate ${ENV_NAME}
+conda install -y -c ilastik ilastik-everything
+
+# install other requirements
 easy_install -U pip
-pip install -r $(realpath $(dirname $0))/requirements.txt
+STARTING_DIR=$(realpath $(dirname $0))
+pip install -r ${STARTING_DIR}/requirements.txt
+
+# fix GUI ilastik bugs
+rm ${CONDA_PREFIX}/lib/libstdc++.*
+REORDER_AXES_PATH=${CONDA_PREFIX}/ilastik-meta/lazyflow/lazyflow/operators
+cd ${REORDER_AXES_PATH}
+cp opReorderAxes.py opReorderAxes.pyBACKUP
+patch -i ${STARTING_DIR}/opReorderAxes.patch
