fd55c2614455442abed3f62ee0f667d9118083e6
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-09-15 15:43:42 -0400

    small skeleton in dev_run

--- skeleton_synapses/dev_run.sh
+++ skeleton_synapses/dev_run.sh
@@ -6,8 +6,9 @@ project_dir=$(realpath "../projects-2017/L1-CNS")
 cred_path="credentials_dev.json"
 stack_id=1
 # skel_id=18531735  # small test skeleton only on CLB's local instance
-skel_id=11524047 # real skeleton
-force=1
+skel_id=19279021  # small test skeleton only on CATSOP instance
+# skel_id=11524047 # real skeleton
+force=0
 
 source ./set_env_vars.sh
 ./locate_syn_catmaid.py ${cred_path} ${stack_id} ${project_dir} ${skel_id} -f ${force}
